39c45084e9d6ed71a058f0a8ca294937caa577f1
horsjaco117 <jacob_horsley@live.com>
2025-02-18 09:12:21 -0700

    Add project files.

---
+++ Roll of the dice/RolloftheDice.vb
@@ -0,0 +1,17 @@
+﻿'Jacob Horsley
+'RCET 0265
+'Spring 2025
+'Roll of the Dice
+'URL:
+
+'[ ] Create array from 2 to 12
+'[ ] Have it roll a thousand time 
+'[ ] Format things into columns and headers 
+'[ ] Ensure the total amount of rolls shows up 
+Module RolloftheDice
+
+    Sub Main()
+
+    End Sub
+
+End Module
