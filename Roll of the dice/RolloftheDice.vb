abb79f082b9d2a6a9485ca183e911527ee38b2c8
horsjaco117 <jacob_horsley@live.com>
2025-02-19 18:59:17 -0700

    Pertinent class notes transferred successfully.

--- Roll of the dice/RolloftheDice.vb
+++ Roll of the dice/RolloftheDice.vb
@@ -11,7 +11,44 @@
 Module RolloftheDice
 
     Sub Main()
+        TestRandomness()
+    End Sub
+
+    Sub TestRandomness()
+        Dim beanCounter(20) As Integer
+
+        For i = 1 To 1000000
+
+            beanCounter(randomNumberBetween(5, 16)) += 1 'the return is the pointer for the array
+
+
+        Next
+
+        For i = LBound(beanCounter) To UBound(beanCounter)
+
+            Console.WriteLine($"{i} hit {beanCounter(i)} times!")
+
+        Next
 
     End Sub
+
+
+    Function randomNumberBetween(max As Integer, min As Integer) As Integer 'We need to declare return type
+        Dim temp As Single 'The single type helps work with the randomize stuff
+        Randomize()
+        temp = Rnd()
+        'establish range 
+        temp *= (max + 1) - min  'supposedly adding the one increase the max by 1. to fix inclusivity of max/min
+
+        temp += min - 1 'This is supposed to shift the min down by 1
+        'Return CInt(temp) 'bad randomness
+        'Return CInt(Int(temp)) 'Randomness is ok, but max isn't included
+        'Return CInt(Math.Floor(temp)) 'randomness is ok, but max isn't included
+        Return CInt(Math.Floor(temp)) 'min isn't included
+        'Return CInt(Math.Ceiling(temp)) 'Always remember a function needs to return
+    End Function
+
+
+
 
 End Module
