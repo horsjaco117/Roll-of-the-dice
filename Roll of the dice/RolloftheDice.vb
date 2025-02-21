9ae11bc595cf4af2468f67aa0be41865e1a626b3
horsjaco117 <jacob_horsley@live.com>
2025-02-21 15:49:06 -0700

    Everything seems to work as far as I can tell.

--- Roll of the dice/RolloftheDice.vb
+++ Roll of the dice/RolloftheDice.vb
@@ -2,18 +2,19 @@
 'RCET 0265
 'Spring 2025
 'Roll of the Dice
-'URL:
+'URL:https://github.com/horsjaco117/Roll-of-the-dice
 
-'[ ] Create array from 2 to 12
-'[ ] Have it roll a thousand time 
-'[ ] Format things into columns and headers 
-'[ ] Ensure the total amount of rolls shows up 
+'[x] Create array from 2 to 12
+'[x] Have it roll a thousand time 
+'[x] Format things into columns and headers 
+'[x] Ensure the total amount of rolls shows up 
+'[x] Randomness
 Module RolloftheDice
 
     Sub Main()
         Dim beanCounter(12) As Integer
-        ' TestRandomness(beanCounter)
-        DisplayBoard(beanCounter)
+        TestRandomness(beanCounter) 'This runs the random number generator
+        DisplayBoard(beanCounter) 'Things are organized into a board
     End Sub
 
     Sub TestRandomness(ByRef beanCounter() As Integer)
@@ -24,36 +25,28 @@ Module RolloftheDice
 
         Next
 
-        For i = 2 To UBound(beanCounter)
-
-            Console.WriteLine($"{i}|{beanCounter(i)}")
-
-        Next
-
     End Sub
 
 
-    Function randomNumberBetween(max As Integer, min As Integer) As Integer 'We need to declare return type
+    Function randomNumberBetween(max As Integer, min As Integer) As Integer
         Dim temp As Single 'The single type helps work with the randomize stuff
         Randomize()
         temp = Rnd()
         temp = temp * (max - min + 1) + min
         Return CInt(Math.Floor(temp)) 'min isn't included
-        'Return CInt(Math.Ceiling(temp)) 'Always remember a function needs to return
     End Function
 
-    Sub DisplayBoard(beanCounter() As Integer)
+    Sub DisplayBoard(diceCounter() As Integer)
         Dim heading() As String = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"}
         Dim columnWidth As Integer = 6 ' Set a consistent width for alignment
-        Console.WriteLine("                          Roll of the Dice")
+        Console.WriteLine("                             Roll of the Dice")
         Console.WriteLine(StrDup((columnWidth + 1) * heading.Length + 1, "-"))
 
-        'Console.Write(heading(0))
-        ' Print header
         Console.Write("|")
         For Each number In heading
             Console.Write(number.PadLeft(columnWidth) & "|")
         Next
+
         Console.WriteLine()
 
         ' Print separator line
@@ -61,8 +54,8 @@ Module RolloftheDice
 
         ' Print values
         Console.Write("|")
-        For i = 2 To UBound(beanCounter)
-            Console.Write(beanCounter(i).ToString().PadLeft(columnWidth) & "|")
+        For i = 2 To UBound(diceCounter)
+            Console.Write(diceCounter(i).ToString().PadLeft(columnWidth) & "|")
         Next
         Console.WriteLine()
     End Sub
